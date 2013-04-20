1bc1ec8030388e1bbb9ca409893527314e72f01a
groteck <groteck@gmail.com>
2013-04-20 13:14:38 +0100

    my first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Inx::Application.config.secret_key_base = '2e2909d86c55bd149f2745b779a7adf5f2013b72f1f6bf0d90ad3ed343fd446af7b4e4378412adbfe61f9956f2c05264d99ba3877879f168eeaaf0aa2bb0916a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Inx::Application.config.session_store :encrypted_cookie_store, key: '_inx_session'
